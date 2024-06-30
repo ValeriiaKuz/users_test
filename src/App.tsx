0b66fcbcc8838f2c949bf1c7ac989285d21d367f
ValeriiaKuz <lerabukareva@icloud.com>
2024-06-30 15:27:08 +0300

    feat: add toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import { UsersPage } from './app/pages/UsersPage/UsersPage';
+import { ToastContainer } from 'react-toastify';
 
 export const App = () => {
   return (
     <>
+      <ToastContainer position={'bottom-right'} autoClose={2000} />
       <UsersPage />
     </>
   );
--- src/app/utiles/saga.ts
+++ src/app/utiles/saga.ts
@@ -11,36 +11,49 @@ import { createUserRequest, deleteUserRequest, usersRequest } from '../API/reque
 import type { UserType } from '../types/userTypes';
 import type { AxiosResponse } from 'axios';
 import type { PayloadAction } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
 function* getUsersSaga() {
   try {
+    toast.loading('Loading...');
     const response: AxiosResponse<UserType[]> = yield call(usersRequest);
     yield put(getUsersSuccessAction(response.data));
+    toast.dismiss();
+    toast.success('Users received');
   } catch (error) {
     if (error instanceof Error) {
       yield put(getUsersErrorAction(error));
+      toast.error(`Something went wrong: ${error.message}`);
     }
   }
 }
 
 function* createUserSaga({ payload: { name, username, email } }: PayloadAction<Omit<UserType, 'id'>>) {
   try {
+    toast.loading('Loading...');
     const response: AxiosResponse<UserType> = yield call(() => createUserRequest({ name, username, email }));
     yield put(createUserSuccessAction(response.data));
+    toast.dismiss();
+    toast.success('User created');
   } catch (error) {
     if (error instanceof Error) {
       yield put(createUserErrorAction(error));
+      toast.error(`Something went wrong: ${error.message}`);
     }
   }
 }
 
 function* deleteUserSaga({ payload: id }: PayloadAction<number>) {
   try {
+    toast.loading('Loading...');
     yield call(() => deleteUserRequest(id));
     yield put(deleteUserSuccessAction(id));
+    toast.dismiss();
+    toast.success('User deleted');
   } catch (error) {
     if (error instanceof Error) {
       yield put(deleteUserErrorAction(error));
+      toast.error(`Something went wrong: ${error.message}`);
     }
   }
 }
