99282959b7073465e8913132c0f5fe2bd3e5a1fa
ValeriiaKuz <lerabukareva@icloud.com>
2024-06-30 13:54:53 +0300

    refactor: replace validation schemes into separate file

---
+++ src/app/helpers/validationSchems.ts
@@ -0,0 +1,16 @@
+import * as Yup from 'yup';
+
+export const UserSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Minimum length is 2 characters')
+    .max(30, 'Maximum length is 30 characters')
+    .required('Field is required'),
+  username: Yup.string()
+    .min(5, 'Minimum length is 5 characters')
+    .max(15, 'Maximum length is 15 characters')
+    .required('Field is required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Incorrect email')
+    .required('Field is required')
+});
